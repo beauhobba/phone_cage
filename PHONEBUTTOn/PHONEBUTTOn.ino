33fcec2a8c8a3eff63ae298874cdf02f76c61cce
beau <hobbabeau@gmail.com>
2022-09-12 19:25:41 +1000

    Added: touch cap sensor

--- PHONEBUTTOn/PHONEBUTTOn.ino
+++ PHONEBUTTOn/PHONEBUTTOn.ino
@@ -1,10 +1,19 @@
 #include "timeManager.h"
-
+#include <CapacitiveSensor.h>
 
 OutputTimer ButtonTimer;
 OutputTimer WorkTimer;
 
+#define PATTING_THRESHOLD 200
+
+
+
+
+// PINS HERE
 const int buttonPin = 8;  
+const int limitSwitchPin = 10; 
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);  
+
 int buttonState = 0;   
 
 
@@ -12,12 +21,20 @@ void setup() {
   // initialize the LED pin as an output:
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  pinMode(limitSwitchPin, INPUT);
   Serial.begin(9600);
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF); 
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
+  Serial.println(digitalRead(limitSwitchPin));
+  long patting =  cs_4_2.capacitiveSensor(30);
+  if(patting > PATTING_THRESHOLD){
+    Serial.println(patting);
+  }
+  
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == LOW && ButtonTimer.available()) {
@@ -29,8 +46,7 @@ void loop() {
 
   if(WorkTimer.available()){
     Serial.println("Timer is Available"); 
-    Serial.println(WorkTimer.time_interval); 
   }
   
-  delay(5);
+  delay(100);
 }
